f28ab1677e7d34d896ceb2e23032a588a337f87e
Thomas Graves <thomas@ooo.pm>
2014-11-03 21:33:16 -0500

    Fixed rubinius install

--- rbx-docker/start-server.sh
+++ rbx-docker/start-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 APPNAME='SampleApp'
 
-cd /var/www/$APPNAME && rake db:create && rake db:migrate && foreman start
+cd /var/www/$APPNAME && rake db:create && rake db:migrate && puma --config config/puma.rb
